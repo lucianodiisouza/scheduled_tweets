0bf43602604e8eab3704625741cc644259bcc491
Luciano dii Souza <lucianodiisouza@hotmail.com>
2022-06-08 22:41:43 -0300

    feat: create sign-up form linked on model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get 'about-us', to: 'about#index', as: :about
+  get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
+
   root to: "main#index"
 end
