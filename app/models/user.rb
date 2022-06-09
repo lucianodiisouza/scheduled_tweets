78ea3fb1c5036fbffff688b7f7e2ec49bcb94592
Luciano dii Souza <lucianodiisouza@hotmail.com>
2022-06-08 22:16:47 -0300

    feat: adding mail validator, update migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@
 # password_confirmation:string virtual
 class User < ApplicationRecord
 	has_secure_password
+
+	validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'invalid email address provided'}
 end
 
